1c28db1ad4b4397c2ff4d994db105b5010332107
Sansala Gangadari <127591252+Gangadari11@users.noreply.github.com>
2024-10-13 20:11:06 +0530

    Add files via upload

---
+++ healthcare2.sql
@@ -0,0 +1,20 @@
+#CREATE DATABASE healthcare2;
+#USE healthcare2;
+
+-- CREATE TABLE heart_disease_data (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     age INT NOT NULL,
+--     sex TINYINT NOT NULL,
+--     cp TINYINT NOT NULL,
+--     trestbps INT NOT NULL,
+--     chol INT NOT NULL,
+--     fbs TINYINT NOT NULL,
+--     restecg TINYINT NOT NULL,
+--     thalach INT NOT NULL,
+--     exang TINYINT NOT NULL,
+--     oldpeak FLOAT NOT NULL,
+--     slope TINYINT NOT NULL,
+--     ca TINYINT NOT NULL,
+--     thal TINYINT NOT NULL,
+--     result TINYINT NOT NULL
+-- );
