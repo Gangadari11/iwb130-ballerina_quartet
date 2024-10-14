19ae6227f5a53ab2ef84d4cdf8245272a738c95d
Lakshani09DL <subhagyal2002@gmail.com>
2024-10-14 13:03:37 +0530

    Add some changes to the frontend

--- Frontend/Model/src/pages/Home.jsx
+++ Frontend/Model/src/pages/Home.jsx
@@ -1,25 +1,35 @@
+// Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Row, Col } from 'react-bootstrap';
 import './Home.css'; // Import your CSS file
+
 
 const Home = () => {
     return (
-        <div className="home-background">
+        <div
+            className="home-background"
+        >
             <Container className="text-center">
                 <h1>Welcome to AI Predictor</h1>
                 <p className="lead">Use our AI model to get predictions based on your inputs!</p>
-                <div>
-                    <Link to="/signup">
-                        <Button variant="primary" className="m-2">Sign Up</Button>
-                    </Link>
-                    <Link to="/login">
-                        <Button variant="secondary" className="m-2">Login</Button>
-                    </Link>
-                    <Link to="/predict">
-                        <Button variant="success" className="m-2">Get Prediction</Button>
-                    </Link>
-                </div>
+                <Row className="justify-content-center">
+                    <Col md="auto">
+                        <Link to="/signup">
+                            <Button variant="primary" className="m-2">Sign Up</Button>
+                        </Link>
+                    </Col>
+                    <Col md="auto">
+                        <Link to="/login">
+                            <Button variant="secondary" className="m-2">Login</Button>
+                        </Link>
+                    </Col>
+                    <Col md="auto">
+                        <Link to="/predict">
+                            <Button variant="success" className="m-2">Get Prediction</Button>
+                        </Link>
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
